8423cd06a110f2f8961836b257a55c4bfe1622b7
amrutn <amrutn@berkeley.edu>
2018-12-17 12:37:25 -0800

    untested low-pass filter

--- lock_in.py
+++ lock_in.py
@@ -2,13 +2,21 @@ import numpy as np
 import os
 import reference_signal as rf
 import struct
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+
 
 #1D Array of wavelength reference for each reading
 wavelengths = np.loadtxt(fname = "wavelengths.csv")
 
+#2D Array of timestamp and intensities for each wavelength
 intensities = np.loadtxt(fname = "intensities.csv")
 
+#Cutoff frequency for low pass filter (Hz)
+cutoff = 0.1
 
+#Order of the butterworth low pass filter
+order = 5
 
 
 i = 0
@@ -26,6 +34,56 @@ while (i < len(intensities)):
         mixed_phaseShift[i].append(ref_value_phaseShift * intensities[i][j])
         j += 1
     i += 1
+
+
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+#returns 0th column of mixed (timestamps in milliseconds)
+time = mixed[:,0]
+
+timeSteps = len(time)
+totalTime = time[timeSteps - 1] - time[0]
+timePerSample = totalTime/timeSteps
+
+#Setting sample rate to fs in Hertz - approximates a constant sample rate
+fs = 1000/timePerSample
+
+#Applies low-pass filter to each column of mixed
+i = 1
+filtered = []
+filtered[:,0] = time
+while (i < len(mixed[0])):
+    data = mixed[:,i]
+    filteredColumn = butter_lowpass_filter(data, cutoff, fs, order)
+    filtered[:, i] = filteredColumn
+    i += 1
+
+#Applies low-pass filter to each column of mixed_phaseShift
+i = 1
+filtered_phaseShift = []
+filtered_phaseShift[:,0] = time
+while (i < len(mixed_phaseShift[0])):
+    data = mixed_phaseShift[:,i]
+    filteredColumn = butter_lowpass_filter(data, cutoff, fs, order)
+    filtered_phaseShift[:, i] = filteredColumn
+    i += 1
+
+x = filtered[:,0]
+y = filtered[:, 1]
+
+
+
 
 #Deletes last line of file
 """def deleteEndLine (file):
