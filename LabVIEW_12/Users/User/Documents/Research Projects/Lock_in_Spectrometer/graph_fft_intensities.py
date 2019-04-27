Finished proof of concept, fixed errors with timestamp units and mixing frequency function when fitting

LabVIEW_12/Users/User/Documents/Research Projects/Lock_in_Spectrometer/graph_fft_intensities.py
@@ -6,13 +6,15 @@ import sys
 import matplotlib.pyplot as plt
 from scipy.signal import freqz
 
+arguments = sys.argv
+
 #1D Array of wavelength reference for each reading
 wavelengths = np.genfromtxt(fname = "wavelengths.csv", delimiter = ",")
 
 #2D Array of timestamp and intensities for each wavelength
-intensities = np.genfromtxt(fname = "intensities.csv", delimiter = ",")
+intensities = np.genfromtxt(fname = arguments[2], delimiter = ",")
 
-arguments = sys.argv
+
 
 #Sampling rate of the reference signal DaQ
 refFreq = float(arguments[1])
@@ -20,22 +22,35 @@ refFreq = float(arguments[1])
 est_freq, est_phase, est_offset, est_amp = rf.setUp(intensities[0][0], refFreq)
 
 def refValue (t):
-    return est_amp * np.sin(est_freq*t+est_phase)
+    return est_amp*np.sin(est_freq*t*2*np.pi + est_phase)
 
+def sin_fn(t, amp, freq, phase):
+    return amp*np.sin(freq*t*2*np.pi + phase)
 
 #Returns value of the reference signal phase shifted by pi/2 rad
+def cos_fn(t, amp, freq, phase):
+    return amp*np.cos(freq*t*2*np.pi + phase)
+
 def refValue_phaseShift (t):
-    return est_amp * np.cos(est_freq*t+est_phase)
+    return est_amp*np.cos(est_freq*t*2*np.pi + est_phase)
 
 #Mixes signal with the reference signal
 i = 0
 mixed = []
 mixed_phaseShift = []
+ref_values = []
+ref_values_halfpi_shift = []
+timeStamps = []
 while (i < len(intensities)):
     j = 1
-    timeStamp = intensities[i][0]
-    ref_value = refValue(timeStamp)
-    ref_value_phaseShift = refValue_phaseShift(timeStamp)
+    timeStamp = intensities[i][0]/1000
+    ref_value = sin_fn(timeStamp, 1, est_freq, 0)
+    ref_value_phaseShift = cos_fn(timeStamp, 1, est_freq, 0)
+    # logging for error checking
+    ref_values.append(ref_value)
+    ref_values_halfpi_shift.append(ref_value_phaseShift)
+    timeStamps.append(timeStamp)
+
     mixed.append([timeStamp])
     mixed_phaseShift.append([timeStamp])
     while (j < len(intensities[i])):
@@ -43,34 +58,44 @@ while (i < len(intensities)):
         mixed_phaseShift[i].append(ref_value_phaseShift * intensities[i][j])
         j += 1
     i += 1
+plt.figure()
+plt.plot(timeStamps, ref_values)
+plt.title("sin function to mix")
+plt.xlabel("Time in Seconds")
+plt.figure()
+plt.plot(timeStamps, ref_values_halfpi_shift)
+plt.title("cos function to mix")
+plt.xlabel("Time in Seconds")
+print("timeStamps:\n\n", timeStamps, "\n\n")
 
 mixed = np.asarray(mixed)
 mixed_phaseShift = np.asarray(mixed_phaseShift)
 #graphs fft of every 100th column of intensities
-i = 1
+"""every incrememnt of i by 100 is equivalent to a ~20 nm increment in wavelength, 
+i = 1 is equivalent to the wavelength ~500 nm"""
+i = 250
 time = intensities[:, 0]
 time = [timestamp/1000 for timestamp in time]
 f_s = (len(time))/(time[-1]-time[0])
 frequencies = np.fft.fftfreq(len(time)) * f_s
-while (i < 1900):
+while (i < 1600):
     data = intensities[:,i]
     transformedColumn = fft(data)
     plt.figure(i + 1)
     plt.plot(frequencies, transformedColumn)
     plt.title(str(wavelengths[i]) + "nm Intensity in Freq Space")
     plt.xlabel("Frequency in Hz")
-    """plt.figure(i + 2)
+    plt.figure(i + 2)
     plt.plot(time, data)
     plt.title(str(wavelengths[i]) + "nm Intensity in Time space")
     plt.xlabel("Time in Seconds")
     plt.figure(i+3)
     plt.plot(time, mixed[:, i])
     plt.title(str(wavelengths[i]) + "nm Mixed in Time Space")
-    """
+    
     plt.figure(i+4)
     plt.plot(frequencies, fft(mixed[:, i]))
     plt.title(str(wavelengths[i]) + "nm Mixed in Freq Space")
-    i += 100
-"""every incrememnt of i by 100 is equivalent to a ~20 nm increment in wavelength, 
-i = 1 is equivalent to the wavelength ~500 nm"""
+    i += 1125
+
 plt.show()

LabVIEW_12/Users/User/Documents/Research Projects/Lock_in_Spectrometer/lock_in.py
@@ -6,7 +6,6 @@ import sys
 
 
 
-
 arguments = sys.argv
 
 #Format of lock_in output values (R, theta) vs (x, y)
@@ -28,87 +27,50 @@ iteration = arguments[5]
 wavelengths = np.genfromtxt(fname = "wavelengths.csv", delimiter = ",")
 
 #2D Array of timestamp and intensities for each wavelength
-if sample_all_data == "T":
-    file_name = "intensities" + iteration + ".csv"
-    intensities = np.genfromtxt(fname = file_name, delimiter = ",")
-else:
-    intensities = np.genfromtxt(fname = "intensities.csv", delimiter = ",")
+try:
+    if sample_all_data == "T":
+        file_name = "intensities" + str(iteration) + ".csv"
+        intensities = np.genfromtxt(fname = file_name, delimiter = ",")
+    else:
+        intensities = np.genfromtxt(fname = "intensities.csv", delimiter = ",")
+except OSError as e:
+    raise e
 
-est_freq, est_phase, est_offset, est_amp = rf.setUp(intensities[0][0], refFreq)
 
-def refValue (t):
-    return est_amp * np.sin(est_freq*t+est_phase)
+
+def refValue (t, est_amp, est_freq, est_phase):
+    return est_amp*np.sin(est_freq*t*2*np.pi + est_phase)
 
 
 #Returns value of the reference signal phase shifted by pi/2 rad
-def refValue_phaseShift (t):
-    return est_amp * np.cos(est_freq*t+est_phase)
+def refValue_phaseShift (t, est_amp, est_freq, est_phase):
+    return est_amp*np.cos(est_freq*t*2*np.pi + est_phase)
 
-#Mixes signal with the reference signal
-i = 0
-mixed = []
-mixed_phaseShift = []
-while (i < len(intensities)):
-    j = 1
-    timeStamp = intensities[i][0]
-    ref_value = refValue(timeStamp)
-    ref_value_phaseShift = refValue_phaseShift(timeStamp)
-    mixed.append([timeStamp])
-    mixed_phaseShift.append([timeStamp])
-    while (j < len(intensities[i])):
-        mixed[i].append(ref_value * intensities[i][j])
-        mixed_phaseShift[i].append(ref_value_phaseShift * intensities[i][j])
-        j += 1
-    i += 1
 
-mixed = np.asarray(mixed)
-mixed_phaseShift = np.asarray(mixed_phaseShift)
 
 
 #Lowpass filter using the fft algorithm
-def fft_lowpass(data, cutoff, fs):
+def fft_lowpass(data, cutoff, f_s, time):
     n = len(data)
     fourier = fft(data)
-    freq_index = int(cutoff * n / np.pi)
-    i = freq_index
-    while(i < len(fourier)):
-        fourier[i] = 0
-        i += 1
+    frequencies = np.fft.fftfreq(len(time)) * f_s
+    index_upper = 0
+    index_lower = 0
+    for index, freq in enumerate(frequencies):
+        if freq > cutoff:
+            index_upper = index
+            break
+
+    for index, freq in enumerate(frequencies):
+        if freq > -1 * cutoff:
+            index_lower = index
+            break
+
+    for index, freq in enumerate(frequencies):
+        if index < index_lower or index > index_upper:
+            fourier[index] = 0
+
     return ifft(fourier)
-
-
-
-#returns 0th column of mixed (timestamps in milliseconds)
-time = mixed[:,0]
-
-timeSteps = len(time)
-totalTime = time[timeSteps - 1] - time[0]
-timePerSample = totalTime/timeSteps
-
-#Setting sample rate to fs in Hertz - approximates a constant sample rate
-fs = 1000/timePerSample
-
-#Applies low-pass filter and averages output for each column of mixed
-i = 1
-#Number of wavelengths.
-n = len(mixed[0])
-filtered = []
-while (i < n):
-    data = mixed[:,i]
-    filteredColumn = fft_lowpass(data, cutoff, fs)
-    value = np.mean(filteredColumn)
-    filtered.append(np.absolute(value))
-    i += 1
-
-#Applies low-pass filter and averages output for each column of mixed_phaseShift
-i = 1
-filtered_phaseShift = []
-while (i < n):
-    data = mixed_phaseShift[:,i]
-    filteredColumn = fft_lowpass(data, cutoff, fs)
-    value = np.mean(filteredColumn)
-    filtered_phaseShift.append(np.absolute(value))
-    i += 1
 
 #Prints output to csv "lock_in_values_x.csv" and "lock_in_values_y.csv" as [last timeStamp], x0, x1... \n
 #and the same for y
@@ -177,15 +139,70 @@ def polarOutput(values, values_phaseShift, wavelengths, time):
     file1.close()
     file2.close()
 
-if (polar == "T"):
-    polarOutput(filtered, filtered_phaseShift, wavelengths, time)
-else:
-    cartesianOutput(filtered, filtered_phaseShift, wavelengths, time)
+
 
 def main():
     """
     Discription Here
     """
+    est_freq, est_phase, est_offset, est_amp = rf.setUp(intensities[0][0], refFreq)
+
+    #Mixes signal with the reference signal
+    i = 0
+    mixed = []
+    mixed_phaseShift = []
+    while (i < len(intensities)):
+        j = 1
+        timeStamp = intensities[i][0]/1000
+        ref_value = refValue(timeStamp, est_amp, est_freq, est_phase)
+        ref_value_phaseShift = refValue_phaseShift(timeStamp, est_amp, est_freq, est_phase)
+        mixed.append([timeStamp])
+        mixed_phaseShift.append([timeStamp])
+        while (j < len(intensities[i])):
+            mixed[i].append(ref_value * intensities[i][j])
+            mixed_phaseShift[i].append(ref_value_phaseShift * intensities[i][j])
+            j += 1
+        i += 1
+
+    mixed = np.asarray(mixed)
+    mixed_phaseShift = np.asarray(mixed_phaseShift)
+
+    #returns 0th column of mixed (timestamps in milliseconds)
+    time = mixed[:,0]
+
+    timeSteps = len(time)
+    totalTime = time[timeSteps - 1] - time[0]
+    timePerSample = totalTime/timeSteps
+
+    #Setting sample rate to fs in Hertz - approximates a constant sample rate
+    fs = 1/timePerSample
+
+    #Applies low-pass filter and averages output for each column of mixed
+    i = 1
+    #Number of wavelengths.
+    n = len(mixed[0])
+    filtered = []
+    while (i < n):
+        data = mixed[:,i]
+        filteredColumn = fft_lowpass(data, cutoff, fs, time)
+        value = np.mean(filteredColumn)
+        filtered.append(np.absolute(value))
+        i += 1
+
+    #Applies low-pass filter and averages output for each column of mixed_phaseShift
+    i = 1
+    filtered_phaseShift = []
+    while (i < n):
+        data = mixed_phaseShift[:,i]
+        filteredColumn = fft_lowpass(data, cutoff, fs, time)
+        value = np.mean(filteredColumn)
+        filtered_phaseShift.append(np.absolute(value))
+        i += 1
+
+    if (polar == "T"):
+        polarOutput(filtered, filtered_phaseShift, wavelengths, time)
+    else:
+        cartesianOutput(filtered, filtered_phaseShift, wavelengths, time)
 
 if __name__ == "__main__":
     main()

LabVIEW_12/Users/User/Documents/Research Projects/Lock_in_Spectrometer/reference_signal.py
@@ -3,16 +3,20 @@
 
 import numpy as np
 from scipy.optimize import leastsq
+import os
 
 def setUp(startTime, refFreq):
-    rawInput = np.genfromtxt(fname = "RefData.csv", delimiter = ",")
+    try:
+        rawInput = np.genfromtxt(fname = "RefData.csv", delimiter = ",")
+    except OSError as e:
+        print("working directory: ", os.getcwd())
+        raise e
     samplingRate = refFreq #Sampling rate of the reference data in Hz
     #Number of data points
     N = len(rawInput)
     #Array of time values for sample
     time = np.linspace(startTime, N/samplingRate, N)
-
-    guess_phase = 0
+    
 
     maxValue = max(rawInput)
     minValue = min(rawInput)
@@ -30,17 +34,30 @@ def setUp(startTime, refFreq):
 
 
     #guesses frequency based on period in the middle of the data
-    i = int(N/2)
-    startingSign = (rawInput[i] - guess_offset > 0)
-    while ((rawInput[i] - guess_offset > 0) == startingSign):
-        i += 1
-    startIndex = i
-    while ((rawInput[i] - guess_offset > 0) != startingSign):
-        i += 1
-    endIndex = i
-
+    try:
+        i = int(N/2)
+        startingSign = (rawInput[i] - guess_offset > 0)
+        while ((rawInput[i] - guess_offset > 0) == startingSign):
+            i += 1
+        startIndex = i
+        while ((rawInput[i] - guess_offset > 0) != startingSign):
+            i += 1
+        endIndex = i
+    except IndexError as e:
+        print("Bad reference signal")
+        raise e
 
     guess_freq = 0.5 * samplingRate/(endIndex - startIndex)
+
+    try:
+        i = 0
+        while ((rawInput[i] - guess_offset > 0) == False):
+            i += 1
+        phaseIndex = i
+    except IndexError as e:
+        print("Bad reference signal")
+        raise e
+    guess_phase = np.pi*(phaseIndex/(endIndex-startIndex))
 
     # we'll use this to plot our first estimate. This might already be good enough for you
     #data_first_guess = guess_amp * np.sin(time+guess_phase) + guess_offset
@@ -48,6 +65,7 @@ def setUp(startTime, refFreq):
 
     # Define the function to optimize, in this case, we want to minimize the difference
     # between the actual data and our "guessed" parameters
-    optimize_func = lambda x: x[3] * np.sin(x[0]*time * 2 * np.pi+x[1]) + x[2] - rawInput
+    optimize_func = lambda x: x[3]*np.sin(x[0]*time*2*np.pi + x[1]) + x[2] - rawInput
     est_freq, est_phase, est_offset, est_amp = leastsq(optimize_func, [guess_freq, guess_phase, guess_offset, guess_amp])[0]
+    print("est_freq = {} est_phase = {} est_offset = {} est_amp = {}".format(est_freq, est_phase, est_offset, est_amp))
     return est_freq, est_phase, est_offset, est_amp